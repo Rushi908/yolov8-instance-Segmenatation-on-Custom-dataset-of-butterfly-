cb710264889c80c7f8e3c8a6750dc5d2e16de024
Rushi908 <99402463+Rushi908@users.noreply.github.com>
2024-01-20 13:00:29 +0530

    first commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# yolov8-instance-Segmenatation-on-Custom-dataset-of-butterfly-
-# yolov8-instance-Segmenatation-on-Custom-dataset-of-butterfly-
-# a
---
+++ predict.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+model=YOLO("best.pt")
+
+#model.predict(source="1.png", show=True, save=True, show_labels=False, show_conf=False, conf=0.5, save_txt=False, save_crop=False, line_width=2,box=True)
+model.predict(yolo model=best.pt mode=predict source=1.png name=yolov8m_seg_infer_valimages exist_ok=True)
+
+#visualize=True,box=True
